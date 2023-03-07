a09baddd55599ed51c553673bb63b239eb92c4d9
Sancara <santiagocaraballo@gmail.com>
2023-03-07 07:48:20 -0300

    add HTML constant

--- Clase 3/main.py
+++ Clase 3/main.py
@@ -1,5 +1,12 @@
 from wsgiref.simple_server import make_server
 
+HTML = """
+<html>
+    <body>
+        <h1>Hola mundo desde el bootcamp de código facilito</h1>
+    </body>
+</html>
+"""
 
 def application(environ, start_response): # WSGI interface
     '''
@@ -14,11 +21,12 @@ def application(environ, start_response): # WSGI interface
     headers = []
 
     start_response(status, headers) # MetaData
-    html = "Hola mundo estamos en el WSGI"
+    #html = "Hola mundo estamos en el WSGI"
 
-    return [ bytes(html, 'UTF-8')] # Client response
+    return [ bytes(HTML, 'UTF-8')] # Client response -> Rendered the constant HTML line 3
     
-PORT = 8000
+PORT = 8000 
+# For convention in python the ports are 8000, 5000, 3000
 
 with make_server('localhost', PORT, application) as server:
     print(f">>> El servidor se encuentra a la escucha en el puerto {PORT}")
