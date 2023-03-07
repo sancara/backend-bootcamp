59285a1b5b12e8ed840326cbf0aec90358bd4d46
Sancara <santiagocaraballo@gmail.com>
2023-03-07 07:40:12 -0300

    main.py: automatic listening on port 8000

--- Clase 3/main.py
+++ Clase 3/main.py
@@ -4,6 +4,10 @@ from wsgiref.simple_server import make_server
 def application(environ, start_response): #Interfaz wsgi
     '''
     environ: variables from request
+        - method
+        - html version
+        - headers
+        - body
     start_response
     '''
     status = '200 OK'
@@ -14,6 +18,8 @@ def application(environ, start_response): #Interfaz wsgi
 
     return [ bytes(html, 'UTF-8')] # Respuesta al cliente
     
+PORT = 8000
 
-server = make_server('localhost', 8000, application)
-server.serve_forever()
+with make_server('localhost', PORT, application) as server:
+    print(f">>> El servidor se encuentra a la escucha en el puerto {PORT}")
+    server.serve_forever()
