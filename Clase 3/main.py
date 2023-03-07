40ece23a19b08cfd94ee83b459c76590eebd3f05
Sancara <santiagocaraballo@gmail.com>
2023-03-07 07:43:10 -0300

    add comments to verbose the code

--- Clase 3/main.py
+++ Clase 3/main.py
@@ -1,7 +1,7 @@
 from wsgiref.simple_server import make_server
 
 
-def application(environ, start_response): #Interfaz wsgi
+def application(environ, start_response): # WSGI interface
     '''
     environ: variables from request
         - method
@@ -10,16 +10,20 @@ def application(environ, start_response): #Interfaz wsgi
         - body
     start_response
     '''
-    status = '200 OK'
+    status = '200 OK' # must go with "OK" at least 4 chars
     headers = []
 
     start_response(status, headers) # MetaData
     html = "Hola mundo estamos en el WSGI"
 
-    return [ bytes(html, 'UTF-8')] # Respuesta al cliente
+    return [ bytes(html, 'UTF-8')] # Client response
     
 PORT = 8000
 
 with make_server('localhost', PORT, application) as server:
     print(f">>> El servidor se encuentra a la escucha en el puerto {PORT}")
     server.serve_forever()
+
+"""server = make_server('localhost', PORT, application)
+print(f">>> El servidor se encuentra a la escucha en el puerto {PORT}")
+server.serve_forever()"""
