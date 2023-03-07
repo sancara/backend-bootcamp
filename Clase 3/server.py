eb6d1c9a6ab4be2c6ec1ffc3e3e9da918f908de1
Sancara <santiagocaraballo@gmail.com>
2023-03-07 12:01:46 -0300

    server.py return index from route

--- Clase 3/server.py
+++ Clase 3/server.py
@@ -2,4 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.run(debug = True)
+@app.route('/')
+def index():
+    return "Hola mundo desde Flask"
+
+app.run(debug = True) # any change on files will restart the server
